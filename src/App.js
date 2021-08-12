df0e5add99b44a23086aefd00f0bd7450927d858
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-08-12 21:12:49 +0300

    add-redux

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from './components/redux/phonebook/phonebook-actions';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import ContactForm from './components/ContactForm/ContactForm';
 import Filter from './components/Filter/Filter';
 import ContactList from './components/ContactList/ContactList';
 
-let isFirstRender = true;
-
 function App() {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  useEffect(() => {
-    if (isFirstRender) {
-      isFirstRender = false;
-      return;
-    }
-
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  useEffect(() => {
-    if (JSON.parse(localStorage.getItem('contacts'))) {
-      setContacts(JSON.parse(localStorage.getItem('contacts')));
-    }
-  }, []);
+  const contacts = useSelector(state => state.contacts.items);
+  const filter = useSelector(state => state.contacts.filter);
+  const dispatch = useDispatch();
 
   const handleChange = e => {
     if (e.target.name === 'filter') {
-      setFilter(e.target.value);
+      dispatch(actions.filterContacts(e.target.value));
       return;
     }
     if (e.target.name === 'name') {
@@ -46,14 +33,12 @@ function App() {
   const handleSubmit = e => {
     e.preventDefault();
 
-    setContacts(() => {
-      if (contacts.some(contact => contact.name.includes(name))) {
-        alert(`${name} is already in contacts`);
-        return;
-      }
+    if (contacts.some(contact => contact.name.includes(name))) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
 
-      return [...contacts, { id: uuidv4(), name, number }];
-    });
+    dispatch(actions.addContact({ name, number }));
 
     setName('');
     setNumber('');
@@ -67,22 +52,9 @@ function App() {
 
   const deleteItem = e => {
     const { id } = e.target;
-    const newContacts = contacts.filter(el => el.id !== id);
 
-    return setContacts(newContacts);
+    dispatch(actions.deleteContact(id));
   };
-
-  // deleteItem = e => {
-  //   const elemIndexToDelete = this.state.contacts.findIndex(
-  //     contact => e.target.id === contact.id,
-  //   );
-  //   return this.setState(prevState => {
-  //     console.log(elemIndexToDelete);
-  //     prevState.contacts.splice(elemIndexToDelete, 1);
-
-  //     return { prevState };
-  //   });
-  // };
 
   return (
     <div className="container">
